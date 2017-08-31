1aa9a39e9f73438cb4a377ab9fa2fe584eb4ad6b
Andrea <andrea@wittrock.us>
2017-08-30 23:55:10 -0500

    Split UAC module into separate repository

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@
  */
 
 import express from 'express';
+import url from 'url';
+import bodyParser from 'body-parser';
+
 import {o} from 'atp-sugar';
 import config from 'atp-config';
 import appConfig from '../config/app';
 
 //Import all modules
-import userModule from 'atp-user';
+import userModule from 'atp-rest-uac';
 
+//Create the list of REST modules
 const modules = [userModule];
 
 //Merge module components
-//  routes are module-dependent and do not need to be in the merged object
-const modulesMerged = o(modules).reduce(
-    (combined, module) => combined.merge(module),
-    o({})
-).raw;
+const modulesMerged = modules.reduce((combined, module) => combined.merge(module), o({})).raw;
 
 //Join default module config
 config.setDefaults(modulesMerged.config);
@@ -27,13 +27,27 @@ console.log("Module config loaded");
 config.setValues(appConfig);
 console.log("App config loaded");
 
-//Add all routes and start the server
+//Create the app and use the JSON body parser for all requests
+const app = express().use(bodyParser.json());
+
+//Add all module routes
 o(modulesMerged.routes)
-    .reduce((server, routes, basePath) => server.use("/" + basePath, routes), express())
-    .use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send(JSON.stringify(err));
+    .reduce((server, routes, basePath) => server.use("/" + basePath, routes), app)
+
+    //Handle 404s by showing the user what they sent
+    .use((request, res, next) => {
+        res.status(404).send({
+            messages: [{type: 'error', msg: 'Route not found'}],
+            debug: {request: {
+                headers: request.headers,
+                method: request.method,
+                url: request.url,
+                query: url.parse(request.url, true).query,
+                body: request.body
+            }}});
     })
+
+    //Start the server
     .listen(3000,  () => {
         console.log('REST server listening on port 3000!');
     });
