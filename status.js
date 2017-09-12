e5a711315e129dfe331679791c49e85be7c467ab
Andrew Wittrock <andy@wittrock.us>
2017-09-12 14:53:37 -0500

    Make status script more robust

--- status.js
+++ status.js
@@ -12,7 +12,7 @@ console.log("");
 fs.readdir("lib/node_modules", (err, files) => {
     Promise.all(files.map(moduleName => new Promise((resolve, reject) => {
         exec("git status", {cwd: "lib/node_modules/" + moduleName}, (err, stdout, stderr) => {
-            const hasChanges = stdout.indexOf("nothing to commit, working tree clean") === -1;
+            const hasChanges = stdout.indexOf("nothing to commit") === -1;
             const needsPush = stdout.indexOf("Your branch is ahead of") !== -1;
             const module = hasChanges ? moduleName.red : needsPush ? moduleName.yellow : moduleName;
             const status = hasChanges ? "->".red : needsPush ? "->".yellow : "";
