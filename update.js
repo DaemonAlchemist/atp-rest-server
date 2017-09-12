d0208d4f9ccc8d0875400040b4930377723740fd
Andrea <andrea@wittrock.us>
2017-09-11 22:04:22 -0500

    Add update script

---
+++ update.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Andy on 9/11/2017.
+ */
+
+const fs = require('fs');
+const {exec} = require('child_process');
+const Promise = require('promise');
+
+console.log("");
+exec("git pull", () => {});
+
+fs.readdir("lib/node_modules", (err, files) => {
+    Promise.all(files.map(moduleName => new Promise((resolve, reject) => {
+        exec("git pull", {cwd: "lib/node_modules/" + moduleName}, (err, stdout, stderr) => {
+            console.log(stdout);
+            resolve();
+        });
+    }))).then(() => {
+        console.log("");
+    });
+});
