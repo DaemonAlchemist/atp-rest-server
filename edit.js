3be6b698430a6f585feed7607d61c5ba8ea4f25f
Andrea <andrea@wittrock.us>
2017-10-17 21:46:16 -0500

    Complete edit script

--- edit.js
+++ edit.js
@@ -7,20 +7,31 @@ const {execSync} = require('child_process');
 const rimraf = require('rimraf');
 
 const moduleName = process.argv[2];
-const editDir = "./lib/node_modules/" + moduleName;
+const editBaseDir = "./lib/node_modules";
+const editDir = editBaseDir + "/" + moduleName;
 
-console.log(moduleName);
+const execOptions = {stdio: 'pipe'};
+
+console.log("")
+console.log("Setting up " + moduleName + " for editing...");
 
 //Delete any existing module from ./lib/node_modules
+console.log("-- Removing existing editable module (if any)...");
 rimraf.sync(editDir);
 
 //Install default module into ./node_modules
-execSync('npm install ' + moduleName);
+console.log("-- Installing module...");
+execSync('npm install ' + moduleName, execOptions);
 
 //Read repo location from default module
+console.log("-- Reading repository URL...");
 const config = require("./node_modules/" + moduleName + "/package.json");
-const repoUrl = config.repository.url;
-console.log(repoUrl);
+const repoUrl = config.repository.url.replace(/^git\+/, "");
+console.log("-- Repo URL: " + repoUrl);
 
 //Checkout repo into ./lib/node_modules
-execSync("git clone " + repoUrl + " .", {cwd: editDir})
+console.log("-- Checking out module into " + editDir + "...");
+execSync("mkdir " + moduleName, {cwd: editBaseDir}, execOptions);
+execSync("git clone " + repoUrl + " .", Object.assign({}, execOptions, {cwd: editDir}));
+
+console.log("-- Done.");
